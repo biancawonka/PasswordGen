e915b723876371850683b1af7f067638ab9dc04e
biancawonka <134649181+biancawonka@users.noreply.github.com>
2023-07-05 16:52:00 -0500

    CSS color updates

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-
-var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
-var password = "";
-for (var i = 0; i < length; i++) {
-  var randomIndex = Math.floor(Math.random() * charset.length);
-  password += charset[randomIndex];
-}
-return password;
-}
-
-// Function to display the generated password on the web page
-function showGeneratedPassword() {
-var passwordLength = 10; // Length of the generated password
-var generatedPassword = generatePassword(passwordLength);
-document.getElementById("password").textContent = generatedPassword;
-}
+// Function to generate a random password
+function generatePassword(length) {
+    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+    var password = "";
+    for (var i = 0; i < length; i++) {
+      var randomIndex = Math.floor(Math.random() * charset.length);
+      password += charset[randomIndex];
+    }
+    return password;
+  }
+  
+  function showGeneratedPassword() {
+    var passwordLength = 20; 
+    var generatedPassword = generatePassword(passwordLength);
+    document.getElementById("password").textContent = generatedPassword;
+  }
+  
