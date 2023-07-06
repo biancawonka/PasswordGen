fed414fbcb098874eb04189265de8fa8bd556799
biancawonka <134649181+biancawonka@users.noreply.github.com>
2023-07-05 21:15:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
-// Function to generate a random password
-function generatePassword(length) {
-    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
-    var password = "";
-    for (var i = 0; i < length; i++) {
-      var randomIndex = Math.floor(Math.random() * charset.length);
-      password += charset[randomIndex];
-    }
-    return password;
+function generatePassword() {
+
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!@#$%^&*";
+
+  var passwordLength = parseInt(prompt("Enter the desired length of the password (between 8 and 128 characters):"));
+
+
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("Invalid password length. Please enter a number between 8 and 128.");
+    return;
   }
-  
-  function showGeneratedPassword() {
-    var passwordLength = 20; 
-    var generatedPassword = generatePassword(passwordLength);
-    document.getElementById("password").textContent = generatedPassword;
+
+
+  var includeLowercase = confirm("Would you like to include lowercase characters?");
+  var includeUppercase = confirm("How about some uppercase characters?");
+  var includeNumeric = confirm("You definitely want some numeric characters, right?");
+  var includeSpecial = confirm("A dash of special characters?");
+
+ 
+  if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+    alert("At least one character type must be selected.");
+    return;
   }
-  
+
+
+  var characters = "";
+  if (includeLowercase) characters += lowercaseChars;
+  if (includeUppercase) characters += uppercaseChars;
+  if (includeNumeric) characters += numericChars;
+  if (includeSpecial) characters += specialChars;
+
+
+  var password = "";
+  for (var i = 0; i < passwordLength; i++) {
+    password += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+
+
+  document.getElementById("passwordTextbox").value = password;
+}
